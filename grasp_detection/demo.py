26a149a6304cfddf9daf4229b9bec828ca672bcd
Peiqi Liu <leo20021210@163.com>
2025-08-20 17:28:00 -0400

    experiment

--- grasp_detection/demo.py
+++ grasp_detection/demo.py
@@ -18,19 +18,35 @@ cfgs = parser.parse_args()
 cfgs.max_gripper_width = max(0, min(0.1, cfgs.max_gripper_width))
 
 def demo(data_dir):
+    import pynvml
+    pynvml.nvmlInit()
+    handle = pynvml.nvmlDeviceGetHandleByIndex(0)  # GPU 0
+    info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+    print(f"Total VRAM: {info.total / 1024**2:.2f} MB")
+    print(f"Used VRAM:  {info.used / 1024**2:.2f} MB")
+    print(f"Free VRAM:  {info.free / 1024**2:.2f} MB")
+    pynvml.nvmlShutdown()
     anygrasp = AnyGrasp(cfgs)
     anygrasp.load_net()
 
     # get data
     colors = np.array(Image.open(os.path.join(data_dir, 'color.png')), dtype=np.float32) / 255.0
-    depths = np.array(Image.open(os.path.join(data_dir, 'depth.png')))
+    print(colors.shape)
+    # depths = np.array(Image.open(os.path.join(data_dir, 'depth.png')))
+    depths = np.load(os.path.join(data_dir, 'depths.npy'))
+    print(depths.max(), depths.min())
     # get camera intrinsics
-    fx, fy = 927.17, 927.37
-    cx, cy = 651.32, 349.62
-    scale = 1000.0
+    fx, fy = 606.56, 606.72
+    # cx, cy = 324, 236.26
+    cx, cy = 236.26, 324
+    scale = 1.0
+    # fx, fy = 927.17, 927.37
+    # cx, cy = 651.32, 349.62
+    # scale = 1000.0
+
     # set workspace to filter output grasps
-    xmin, xmax = -0.19, 0.12
-    ymin, ymax = 0.02, 0.15
+    xmin, xmax = -0.2, 0
+    ymin, ymax = 0, 0.2
     zmin, zmax = 0.0, 1.0
     lims = [xmin, xmax, ymin, ymax, zmin, zmax]
 
@@ -42,13 +58,26 @@ def demo(data_dir):
     points_y = (ymap - cy) / fy * points_z
 
     # set your workspace to crop point cloud
-    mask = (points_z > 0) & (points_z < 1)
+    mask = (points_z > 0) & (points_z < float('inf'))
     points = np.stack([points_x, points_y, points_z], axis=-1)
     points = points[mask].astype(np.float32)
     colors = colors[mask].astype(np.float32)
     print(points.min(axis=0), points.max(axis=0))
 
+    import time
+    start_time = time.time()
+
+    import pynvml
+    pynvml.nvmlInit()
+    handle = pynvml.nvmlDeviceGetHandleByIndex(0)  # GPU 0
+    info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+    print(f"Total VRAM: {info.total / 1024**2:.2f} MB")
+    print(f"Used VRAM:  {info.used / 1024**2:.2f} MB")
+    print(f"Free VRAM:  {info.free / 1024**2:.2f} MB")
+    pynvml.nvmlShutdown()
     gg, cloud = anygrasp.get_grasp(points, colors, lims=lims, apply_object_mask=True, dense_grasp=False, collision_detection=True)
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time} seconds")
 
     if len(gg) == 0:
         print('No Grasp detected after collision detection!')
@@ -56,17 +85,58 @@ def demo(data_dir):
     gg = gg.nms().sort_by_score()
     gg_pick = gg[0:20]
     print(gg_pick.scores)
+    print("translation: ", gg_pick[0].translation)
+    print("rotation_matrix: ", gg_pick[0].rotation_matrix)
+    print("depth: ", gg_pick[0].depth)
+    print("width: ", gg_pick[0].width)
     print('grasp score:', gg_pick[0].score)
 
     # visualization
     if cfgs.debug:
-        trans_mat = np.array([[1,0,0,0],[0,1,0,0],[0,0,-1,0],[0,0,0,1]])
+        # trans_mat = np.array([[1,0,0,0],[0,1,0,0],[0,0,-1,0],[0,0,0,1]])
+        trans_mat = np.array([[1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,1]])
         cloud.transform(trans_mat)
         grippers = gg.to_open3d_geometry_list()
         for gripper in grippers:
             gripper.transform(trans_mat)
-        o3d.visualization.draw_geometries([*grippers, cloud])
-        o3d.visualization.draw_geometries([grippers[0], cloud])
+        # o3d.visualization.draw_geometries([*grippers, cloud])
+        # o3d.visualization.draw_geometries([grippers[0], cloud])
+
+        save_file = "grasp_all.png"
+
+        visualizer = o3d.visualization.Visualizer()
+        visualizer.create_window(visible=False)
+        for gripper in grippers:
+            visualizer.add_geometry(gripper)
+        visualizer.add_geometry(cloud)
+        visualizer.poll_events()
+        visualizer.update_renderer()
+
+        view_control = visualizer.get_view_control()
+        zoom_scale_factor = 1.4
+        view_control.scale(zoom_scale_factor)
+
+        visualizer.capture_screen_image(save_file, do_render=True)
+        print(f"Saved screen shot visualization at {save_file}")
+        visualizer.destroy_window()
+
+        save_file = "grasp_single.png"
+
+        visualizer = o3d.visualization.Visualizer()
+        visualizer.create_window(visible=False)
+        gripper = grippers[0]
+        visualizer.add_geometry(gripper)
+        visualizer.add_geometry(cloud)
+        visualizer.poll_events()
+        visualizer.update_renderer()
+
+        view_control = visualizer.get_view_control()
+        zoom_scale_factor = 1.4
+        view_control.scale(zoom_scale_factor)
+
+        visualizer.capture_screen_image(save_file, do_render=True)
+        print(f"Saved screen shot visualization at {save_file}")
+        visualizer.destroy_window()
 
 
 if __name__ == '__main__':
